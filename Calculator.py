0e55147c97514d176f92dbf96a57753904ddd7bf
AlephKnot2599 <158862150+AlephKnot2599@users.noreply.github.com>
2024-02-17 23:18:14 -0500

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,14 @@
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+operation = input("Enter the operation: ")
+
+if operation == "+":
+    print(number1 + number2)
+elif operation == "-":
+    print(number1 - number2)
+elif operation == "*":
+    print(number1 * number2)
+elif operation == "/":
+    print(number1 / number2)
+else:
+    print("Invalid operation!")
